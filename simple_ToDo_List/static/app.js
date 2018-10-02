56af233e4f692448ce29c16735bf490911768dd1
Judith Mueni <judithmueni395@gmail.com>
2018-10-02 09:28:05 +0300

    To_Do list

---
+++ simple_ToDo_List/static/app.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    todos: [	
+    	"My first to do item",
+    	"My second to do item",
+    	"My third to do item"
+    ],
+    item: 'To do item'
+  },
+  methods: {
+  	addTodo() {
+  		this.todos.push(this.item)
+  		this.item = '';
+  	}
+  }
+})
